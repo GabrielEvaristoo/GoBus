996a9e94a26ddec8ee10e08e828f69c0095add62
GabrielEvaristoo <gabrielevaristoo@gmail.com>
2023-06-29 17:10:22 -0300

    teste de merge:ok

--- 5g/src/main/java/br/com/g/entity/DriveEntity.java
+++ 5g/src/main/java/br/com/g/entity/DriveEntity.java
@@ -3,6 +3,6 @@ package br.com.g.entity;
 public class DriveEntity {
     public static void main(String args[]){
         System.out.println("pode apagar, era so pra testar as classes");
-        String nome ="Gabriel" ;
+        String nome ="Gabriel Evaristo" ;
     }
 }
