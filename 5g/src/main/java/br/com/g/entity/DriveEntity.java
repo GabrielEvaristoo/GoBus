b66944e39d2d368fabcd58633abe25b647fef6e2
GabrielEvaristoo <gabrielevaristoo@gmail.com>
2023-06-29 16:59:19 -0300

    criando branch chamada desenvolvimento para teste de features

--- 5g/src/main/java/br/com/g/entity/DriveEntity.java
+++ 5g/src/main/java/br/com/g/entity/DriveEntity.java
@@ -3,5 +3,6 @@ package br.com.g.entity;
 public class DriveEntity {
     public static void main(String args[]){
         System.out.println("pode apagar, era so pra testar as classes");
+        String nome ="Gabriel" ;
     }
 }
